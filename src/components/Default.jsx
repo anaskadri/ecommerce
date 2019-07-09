6a4e95c83347b4486359cff94ea37927b257365b
anaskadri <anaskadri0@gmail.com>
2019-07-09 10:54:17 +0100

    1

--- src/components/Default.jsx
+++ src/components/Default.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
 class Default extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
     render() { 
         return ( 
-            <div>
-                <h3>PAge not found</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                        <h1 className="display-3">404</h1>
+                        <h1>error</h1>
+                        <h2>page not found</h2>
+                        <h3>The requested URL was not found>
+                            <span className="text-danger">
+                                {this.props.location.pathname}
+                            </span>{" "}
+                            was not found
+                        </h3>
+                    </div>
+                </div>
             </div>
          );
     }
